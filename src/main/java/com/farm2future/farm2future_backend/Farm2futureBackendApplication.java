285b87573833760ae75fd37571f0ba82a5be777b
yinsizhe-spec <ysz2912481870@gmail.com>
2026-06-26 11:54:16 +0800

    初始化仓库

---
+++ src/main/java/com/farm2future/farm2future_backend/Farm2futureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farm2future.farm2future_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm2futureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farm2futureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farm2future/farm2future_backend/Farm2futureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farm2future.farm2future_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farm2futureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
